755f6172a01023dde9bec09e606d0b726bdb03ca
Dustin Bowers <dustin.uncc@gmail.com>
2020-01-12 07:12:00 -0500

    add decipher

--- viginere.py
+++ viginere.py
@@ -2,25 +2,13 @@
 
 import sys
 
-key = ''
-inphrase = ''
-output = ''
-
 ord_start = ord('A')
 grid = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 grid_len = len(grid)
 
-def process_stdin():
-    print(sys.argv)
-    argv = sys.argv
-    global key, inphrase
-    key = argv[1]
-    inphrase = argv[2]
-    print "keyphrase: {0}\ninput: {1}\nord_start {2}".format(key, inphrase, ord_start)
-
 def cipher(key, message, direction=0):
-    output = [None] * len(inphrase)
-    inphrase_upper = inphrase.upper()
+    output = [None] * len(message)
+    inphrase_upper = message.upper()
     key_upper = key.upper()
     print "key_upper {0}".format(key_upper)
     
@@ -29,6 +17,7 @@ def cipher(key, message, direction=0):
         i = 0
         keylen = len(key)
         print "keylen = {0}".format(keylen)
+        print "inphrase = {0}".format(inphrase_upper)
         for c in inphrase_upper:
             print "===cipher start {0} ====".format(i)
             char_i = ord(inphrase_upper[i])
@@ -42,15 +31,50 @@ def cipher(key, message, direction=0):
             output[i] = code
             i = i+1
             print "===cipher end ===\n"
-        print output
+        #print output
 
     else:
         print "direction backward\n"
+        i = 0
+        keylen = len(key)
+        print "keylen = {0}".format(keylen)
+        print "inphrase = {0}".format(inphrase_upper)
+        for c in inphrase_upper:
+            print "===cipher start {0} ====".format(i)
+            char_i = ord(inphrase_upper[i])
+            char_index = char_i - ord_start
+            key_char = key_upper[i % keylen]
+            key_index = grid.index(key_char)
+            print "char_i {0}\nchar_index {1}\nkey_char {2}\nkey_index {3}".format(char_i, char_index, key_char, key_index)
+            grid_offset = (char_index - key_index) % grid_len
+            code = grid[grid_offset]
+            print "grid offset: {0} - code {1}".format(grid_offset, code)
+            output[i] = code
+            i = i+1
+            print "===cipher end ===\n"
+        #print output
+    return ''.join(output)
+
 
 def main():
-    process_stdin()
+    # process command line args
+    print(sys.argv)
+    argv = sys.argv
+    key = argv[1]
+    inphrase = argv[2]
+    print "keyphrase: {0}\ninput: {1}\nord_start {2}".format(key, inphrase, ord_start)
+
     print "main key {0}".format(key)
-    cipher(key, inphrase)
+    outphrase = cipher(key, inphrase)
+    print "ciphertext {0}.".format(outphrase)
+    print "\n\n\n\n"
+    decoded = cipher(key, outphrase, 1)
+    print "\n\n\n\n === final output ===\n\n"
+    print "ciphertext {0}.".format(outphrase)
+    print "deciphered {0}.".format(decoded)
 
 if __name__ == "__main__":
     main()
+
+
+
